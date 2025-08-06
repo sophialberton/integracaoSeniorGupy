559191c494477491f9822fb203814b3195f8b907
sophialberton <sophia.alberton@gmail.com>
2025-08-06 14:57:14 -0300

    test: loggings

--- src/scripts/conexaoGupy.py
+++ src/scripts/conexaoGupy.py
@@ -12,19 +12,36 @@ class conexaoGupy():
     def __init__(self):
         self.token = os.getenv("token")     
     
-    def listaUsuariosGupy(self,emailSenior):
+    
+    
+    def listaUsuariosGupy(self, nomeSenior, emailSenior):
         url = f"https://api.gupy.io/api/v1/users?email={emailSenior}&perPage=10&page=1"
         headers = {
             "accept": "application/json",
             "authorization": f"Bearer {self.token}"
         } 
-        response = requests.get(url,  headers=headers)
+        response = requests.get(url, headers=headers)
         data = response.json()
         detalhe = data.get("detail", "Erro desconhecido")
-        if response.status_code == 201:
-            logging.info(f">Listando usuario na gupy: {emailSenior} (verificaColaboradores.api.criaUsuarioGupy)")
+        
+        if response.status_code == 200:
+            usuarios = data.get("results", [])
+            if usuarios:
+                user_id = usuarios[0].get("id")
+                print(f">Listou usuario {nomeSenior} com email {emailSenior} na GUPY")
+                logging.info(f">Listou usuario {nomeSenior} com email {emailSenior} na GUPY (verificaColaboradores.api.listaUsuariosGupy)")
+                return user_id
+            else:
+                print(f">Listou usuario {nomeSenior} com email {emailSenior} na GUPY")
+                logging.info(f">Listou usuario {nomeSenior} com email {emailSenior} na GUPY (verificaColaboradores.api.listaUsuariosGupy)")
+                return None
         if response.status_code == 400:
-            logging.error(f"> '{detalhe}' >> Usuario: {emailSenior}, (verificaColaboradores.api.criaUsuarioGupy)")
+            print(f">WARNING: '{detalhe}'>> Usuario > Nome: {nomeSenior}; Email: {emailSenior}")
+            logging.warning(f"> '{detalhe}' >> Usuario: {nomeSenior, emailSenior}, (verificaColaboradores.api.criaUsuarioGupy)")
+        else:
+            logging.error(f"> Erro ao listar usuario: {detalhe}")
+            return None
+
 
    
     def criaUsuarioGupy(self,nomeSenior,emailSenior,cpfSenior):
@@ -47,21 +64,21 @@ class conexaoGupy():
         if response.status_code == 400:
             print(f">WARNING: '{detalhe}'>> Usuario > Cpf: {cpfSenior}; Nome: {nomeSenior}; Email: {emailSenior}")
             logging.warning(f"> '{detalhe}' >> Usuario: {nomeSenior, emailSenior}, (verificaColaboradores.api.criaUsuarioGupy)")
-                          
     
-    def deletaUsuarioGupy(self,idGupy, nomeSenior):
-        url = f"https://api.gupy.io/api/v1/users/{idGupy}"
-        headers = {
-            "accept": "application/json",
-            "authorization": f"Bearer {self.token}"
-        }
-        response = requests.delete(url, headers=headers)
-        data = response.json()
-        detalhe = data.get("detail", "Erro desconhecido")
-        if response.status_code == 201:
-            print(f">Deletou usuario desligado: {idGupy, nomeSenior}")
-            logging.info(f">Deletou usuario desligado: {idGupy, nomeSenior} (verificaColaboradores.api.deletaUsuarioGupy)")
-        if response.status_code == 400:
-            print(f">WARNING: '{detalhe}' >> Usuario: {idGupy, nomeSenior}")
-            logging.warning(f"> '{detalhe}' >> Usuario: {idGupy, nomeSenior}, (verificaColaboradores.api.deletaUsuarioGupy)")
-  
+    def deletaUsuarioGupy(self, idGupy, nomeSenior):
+        # url = f"https://api.gupy.io/api/v1/users/{idGupy}"
+        # headers = {
+        #     "accept": "application/json",
+        #     "authorization": f"Bearer {self.token}"
+        # }
+        # response = requests.delete(url, headers=headers)
+        # data = response.json()
+        # detalhe = data.get("detail", "Erro desconhecido")
+        # if response.status_code == 201:
+        #     print(f">Deletou usuario desligado: {nomeSenior, idGupy}")
+        #     logging.info(f">Deletou usuario desligado: {nomeSenior, idGupy} (verificaColaboradores.api.deletaUsuarioGupy)")
+        # if response.status_code == 400:
+        #     print(f">WARNING: '{detalhe}' >> Usuario: {nomeSenior, idGupy}")
+        #     logging.warning(f"> '{detalhe}' >> Usuario: {nomeSenior, idGupy}, (verificaColaboradores.api.deletaUsuarioGupy)")
+        print(f">Chamou o delete para usuario desligado: {nomeSenior, idGupy}")
+        logging.critical(f">Chamou o DELETE para usuario desligado: {nomeSenior, idGupy}")
